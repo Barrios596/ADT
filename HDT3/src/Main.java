946858c6bd455b1e78adaf6b827d8b074bfd7cea
Barrios596 <barrios_596@hotmail.com>
2016-07-27 11:15:48 -0600

    Add files via upload

---
+++ HDT3/src/Main.java
@@ -0,0 +1,18 @@
+/*Universidad del Valle de Guatemala
+* Algoritmos y Estructuras de datos - Seccion 30
+* Rodrigo Barrios, carne 15009
+* Mario Hernandez, carne 15135
+
+* Main.java
+* Clase que utiliza a sorteador para ordenar un listado de numeros en orden ascendente
+*/
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ HDT3/src/Sorteador.java
@@ -0,0 +1,11 @@
+/*Universidad del Valle de Guatemala
+* Algoritmos y Estructuras de datos - Seccion 30
+* Rodrigo Barrios, carne 15009
+* Mario Hernandez, carne 15135
+
+* Sorteador.java
+* Clase que contiene los metodos necesarios para ordenar numeros en orden ascendente
+*/
+public class Sorteador {
+    
+}
